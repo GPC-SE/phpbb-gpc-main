be0ca8cf9c1d6de3aff94288f2b85341377b9efd
Robert Heim <robert@robert-heim.de>
2014-09-29 18:09:08 +0200

    fix ext name

--- ext.php
+++ ext.php
@@ -1,13 +1,13 @@
 <?php
 /**
 *
-* @package phpBB Extension - GPC Indexpage
+* @package phpBB Extension - GPC Main
 * @copyright (c) 2014 Robet Heim
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
-namespace gpc\indexpage;
+namespace gpc\main;
 
 /**
 * @ignore
