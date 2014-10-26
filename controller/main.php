0bb050d2907d6553b76df1648950cd74904e0a9d
Robert Heim <robert@robert-heim.de>
2014-10-26 21:04:54 +0100

    further work on tag-search

--- controller/main.php
+++ controller/main.php
@@ -228,54 +228,156 @@ class main
 	 */
 	public function tutorials_tricks_families()
 	{
+		$search_around_url = $this->helper->route(
+			'gpc_main_controller_tutorials_search', array(
+				'tags' => 'around'
+			));
+		$search_around_url = $this->remove_community($search_around_url);
+		
 		$this->template->assign_vars(array(
-			'S_GPC_TUTORIALS_ACTIVE' => true,
+			'S_GPC_TUTORIALS_ACTIVE'	=> true,
+			'U_GPC_SEARCH_AROUND'		=> $search_around_url,
 		));
 		return $this->helper->render('tricks_families.html');
 	}
 
 	/**
 	 * Controller for route /tutorials/search
+	 * shows a list of topics that have the given $tags assigned
 	 *
+	 * @param $tags tags seperated by comma (",")
+	 * @param $mode the mode indicates whether all tags (AND, default) or any tag (OR) should be assigned to the resulting topics
+	 * @param casesensitive wether to search case-sensitive (true) or -insensitive (false, default)
 	 * @return \Symfony\Component\HttpFoundation\Response A Symfony Response object
 	 */
-	public function tutorials_search()
+	public function tutorials_search($tags, $mode, $casesensitive)
 	{
+		global $user, $phpbb_container, $config, $phpbb_root_path, $request;
+		$form_action_route = $this->helper->route(
+			'gpc_main_controller_tutorials_search', array(
+				'tags' => ''
+			));
+		$form_action_route = $this->remove_community($form_action_route);
 		$this->template->assign_vars(array(
-			'S_GPC_TUTORIALS_ACTIVE' => true,
-			'S_RH_TOPICTAGS_INCLUDE_NG_TAGS_INPUT'		=> true,
-			'S_RH_TOPICTAGS_INCLUDE_CSS'				=> true,				
+			'S_GPC_TUTORIALS_ACTIVE'				=> true,
+			'S_RH_TOPICTAGS_INCLUDE_NG_TAGS_INPUT'	=> true,
+			'S_RH_TOPICTAGS_INCLUDE_CSS'			=> true,
+			'U_SEARCH_ROUTE'						=> $form_action_route,
 		));
 
 		// TODO
-		$tags = array('beginner', 'trick', 'pen');
+		$tag_suggestions = array('beginner', 'trick', 'pen');
 		
-	for ($i = 0, $size = sizeof($tags); $i < $size; $i++)
+		for ($i = 0, $size = sizeof($tag_suggestions); $i < $size; $i++)
 		{
 			$this->template->assign_block_vars('rh_topictags_suggestions', array(
 				'LINK' => '#',
-				'NAME' => $tags[$i],
+				'NAME' => $tag_suggestions[$i],
 			));
 		}
 		
-		$tutotials = array(
-			array(
-				'link' => '#',
-				'tags' => 'asdf, asdfe',
-			),
-			array(
-				'link' => '#',
-				'tags' => 'asdf',
-			),
-		);
-		for ($i = 0, $size = sizeof($tutotials); $i < $size; $i++)
+		// validate mode
+		// default == AND
+		$mode = ($mode == 'OR' ? 'OR' : 'AND');
+	
+		$tags = explode(',', urldecode($tags));
+		// remove possible duplicates
+		$tags = array_unique($tags);
+		$all_tags = $this->tags_manager->split_valid_tags($tags);
+			
+		$tutorials = array();
+		$tags = $all_tags['valid'];
+		if (sizeof($tags) > 0)
 		{
-			$this->template->assign_block_vars('tutorial', array(
-				'TITLE' => 'A title ' . $i,
-				'LINK' => $tutotials[$i]['link'],
-				'TAGS' => $tutotials[$i]['tags'],
-			));
+			$tags_string = join(', ', $tags);
+			
+			$this->template->assign_var('RH_TOPICTAGS', 
+				base64_encode(json_encode($tags)));
+			
+			$pagination = $phpbb_container->get('pagination');
+			
+			$start = $request->variable('start', 0);
+			$limit = 100; // $config['topics_per_page'];
+			
+			$topics_count = $this->tags_manager->count_topics_by_tags($tags, 
+				$start, $limit, $mode, $casesensitive);
+			$start = $pagination->validate_start($start, 
+				$config['topics_per_page'], $topics_count);
+			
+			$topics = $this->tags_manager->get_topics_by_tags($tags, $start, 
+				$limit, $mode, $casesensitive);
+			
+			$base_url = $this->helper->route(
+				'robertheim_topictags_show_tag_controller', 
+				array(
+					'tags' => urlencode($tags_string)
+				));
+			$base_url = $this->remove_community($base_url);
+			
+			// $pagination->generate_template_pagination($base_url, 'pagination', 'start',
+			// $topics_count, $config['topics_per_page'], $start);
+			
+			$user->add_lang('viewforum');
+			// $this->template->assign_var('TOTAL_TOPICS', $user->lang('VIEW_FORUM_TOPICS',
+			// $topics_count));
+			
+			global $phpEx, $auth, $phpbb_dispatcher, $template;
+			
+			$phpbb_content_visibility = $phpbb_container->get(
+				'content.visibility');
+			include_once ($phpbb_root_path . 'includes/functions_display.' .
+				 $phpEx);
+			
+			foreach ($topics as $topic)
+			{
+				$topic_id = $topic['topic_id'];
+				$row = $topic;
+				$s_type_switch = 0;
+				
+				$topic_forum_id = ($row['forum_id']) ? (int) $row['forum_id'] : $forum_id;
+				
+				// This will allow the style designer to output a different header
+				// or even separate the list of announcements from sticky and normal topics
+				$s_type_switch_test = ($row['topic_type'] == POST_ANNOUNCE ||
+					 $row['topic_type'] == POST_GLOBAL) ? 1 : 0;
+				
+				// Replies
+				$replies = $phpbb_content_visibility->get_count('topic_posts', 
+					$row, $topic_forum_id) - 1;
+				
+				if ($row['topic_status'] == ITEM_MOVED)
+				{
+					$topic_id = $row['topic_moved_id'];
+					$unread_topic = false;
+				}
+				else
+				{
+					$unread_topic = (isset($topic_tracking_info[$topic_id]) &&
+						 $row['topic_last_post_time'] >
+						 $topic_tracking_info[$topic_id]) ? true : false;
+				}
+				// Generate all the URIs ...
+				$view_topic_url_params = 'f=' . $row['forum_id'] . '&amp;t=' .
+					 $topic_id;
+				$view_topic_url = append_sid(
+					"{$phpbb_root_path}viewtopic.$phpEx", $view_topic_url_params);
+				
+				$tutorials[] = array(
+					'title' => censor_text($row['topic_title']),
+					'link' => $view_topic_url,
+					'topic_id' => $topic_id,
+					'tags' => join(', ', 
+						$this->tags_manager->get_assigned_tags($topic_id)),
+					'replies' => $replies,
+					'views' => $row['topic_views'],
+					'author' => get_username_string('full', 
+						$row['topic_poster'], $row['topic_first_poster_name'], 
+						$row['topic_first_poster_colour'])
+				);
+			}
 		}
+		$this->template->assign_var('TUTORIALS', base64_encode(json_encode($tutorials)));
+
 		return $this->helper->render('tag_search.html');
 	}
 
---
+++ styles/all/angular/search.app.js
@@ -0,0 +1,67 @@
+$.fn.flash = function(duration, iterations) {
+    duration = duration || 1000; // Default to 1 second
+    iterations = iterations || 1; // Default to 1 iteration
+    var iterationDuration = Math.floor(duration / iterations);
+
+    for (var i = 0; i < iterations; i++) {
+        this.fadeOut(iterationDuration).fadeIn(iterationDuration);
+    }
+    return this;
+}
+
+angular.module('searchApp', ['ngTagsInput'])
+	.config(function($interpolateProvider, $httpProvider) {
+		$interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+		$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	})
+	.controller('searchCtrl', function($scope, $http) {
+		$scope.tags = [];
+		$scope.tutorials = [];
+		$scope.init = function (initTags, initTutorials) {
+			if ('' != initTags) {
+				initTags = JSON.parse(atob(initTags));
+				for (var i = 0; i < initTags.length; i++) {
+					this.tags.push(initTags[i]);
+				}
+			}
+			if ('' != initTutorials) {
+				initTutorials = JSON.parse(atob(initTutorials));
+				for (var i = 0; i < initTutorials.length; i++) {
+					this.tutorials.push(initTutorials[i]);
+				}
+			}
+		}
+		$scope.loadTags = function(query) {
+			var data = {
+				'query': query,
+				'exclude' : $scope.tags.map(function(tag) {
+					return tag.text;
+				})
+			};
+			return $http.post('/tags/suggest', data);
+		};
+		$scope.addTag = function(tag) {
+			var found = false;
+			this.tags.every(function(element, index, array) {
+				if (element.text == tag) {
+					found = true;
+					return false;
+				}
+				return true;
+			});
+			if (!found) {
+				this.tags.push({"text": tag});
+			} else {
+				$("span:contains('"+tag+"')")
+				.filter(function() {
+				    return $(this).text() === tag;
+				})
+				.parent()
+				.flash(200, 3);
+			}
+		}
+		$scope.jsonRep = '';
+		$scope.$watch('tags', function(t) {
+			$scope.jsonRep = btoa(JSON.stringify(t));
+		}, true);
+	});
