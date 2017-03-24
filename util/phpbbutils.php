263aa678f46266c1045f6f668e259d0ef736d348
Robert Heim <Robert.Heim@rwth-aachen.de>
2017-03-24 20:23:59 +0100

    Update phpbbutils.php

--- util/phpbbutils.php
+++ util/phpbbutils.php
@@ -10,6 +10,7 @@
 namespace gpc\main\util;
 
 class phpbbutils {
+
 	/**
 	 * Trims bbcode containing text to $limit characters by removing all bbcodes.
 	 *
@@ -24,6 +25,8 @@ class phpbbutils {
 	public static function make_preview_text_from_post($post_text, $bbcode_uid, $limit)
 	{
 		$preview = $post_text;
+		// remove youtube completely
+		$preview = trim(preg_replace('/(\[youtube\])(.*)(\[\/youtube\])/i', '', $preview));
 		$preview = self::bbcodeStripping($preview, $bbcode_uid);
 		$preview = trim(preg_replace('#http(?:\:|&\#58;)//\S+#', '', $preview));
 		// Decide how large the preview text should be
@@ -62,21 +65,10 @@ class phpbbutils {
 	 * @param  string $uid  The $uid used in encoding/decoding the bbcode
 	 * @return string $text The post with bbcodes stripped
 	 * @static
-	 * @author MichaelC
 	 */
-	private static function bbcodeStripping($text, $uid = '[0-9a-z]{5,}')
+	private static function bbcodeStripping($text, $uid)
 	{
-		$text = preg_replace("#\[\/?[a-z0-9\*\+\-]+(?:=(?:&quot;.*&quot;|[^\]]*))?(?::[a-z])?(\:$uid)\]#", ' ', $text);
-		$match = array(
-			'#<!\-\- e \-\-><a href="mailto:(.*?)">.*?</a><!\-\- e \-\->#',
-			'#<!\-\- l \-\-><a (?:class="[\w-]+" )?href="(.*?)(?:(&amp;|\?)sid=[0-9a-f]{32})?">.*?</a><!\-\- l \-\->#',
-			'#<!\-\- ([mw]) \-\-><a (?:class="[\w-]+" )?href="(.*?)">.*?</a><!\-\- \1 \-\->#',
-			'#<!\-\- s(.*?) \-\-><img src="\{SMILIES_PATH\}\/.*? \/><!\-\- s\1 \-\->#',
-			'#<!\-\- .*? \-\->#s',
-			'#<.*?>#s',
-		);
-		$replace = array('\1', '\1', '\2', '\1', '', '');
-		$text = preg_replace($match, $replace, $text);
-		return $text;
+
+		return strip_bbcode($text, $uid);
 	}
 }
