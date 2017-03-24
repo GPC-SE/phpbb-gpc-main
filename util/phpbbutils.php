2aed775d1430e840fd14138533d871543a007e81
Robert Heim <Robert.Heim@rwth-aachen.de>
2017-03-24 21:14:42 +0100

    Update phpbbutils.php

--- util/phpbbutils.php
+++ util/phpbbutils.php
@@ -16,19 +16,23 @@ class phpbbutils {
 	 *
 	 * @param String $post_text the text of the post to trim
 	 * @param String $bbcode_uid the bbcode_uid of the post row
+	 * @param String $bbcode_bitfield the bbcode_bitfield of the post row
 	 * @param int $limit the maximum length of the preview text
 	 * @return the trimmed preview text.
 	 *
 	 * @static
 	 * @author MichaelC, Robert Heim
 	 */
-	public static function make_preview_text_from_post($post_text, $bbcode_uid, $limit)
+	public static function make_preview_text_from_post($post_text, $bbcode_uid, $bbcode_bitfield, $limit)
 	{
 		$preview = $post_text;
 		// remove youtube completely
 		//$preview = trim(preg_replace('/(\[youtube\])(.*)(\[\/youtube\])/i', '', $preview));
-		$preview = self::bbcodeStripping($preview, $bbcode_uid);
-		$preview = trim(preg_replace('#http(?:\:|&\#58;)//\S+#', '', $preview));
+		//$preview = self::bbcodeStripping($preview, $bbcode_uid);
+		$bbcode_options = OPTION_FLAG_BBCODE + OPTION_FLAG_SMILIES + OPTION_FLAG_LINKS;
+		$preview = generate_text_for_display($preview, $bbcode_uid, $bbcode_bitfield, $bbcode_options);
+		$preview = self::cut_html($preview, $limit);
+		/*$preview = trim(preg_replace('#http(?:\:|&\#58;)//\S+#', '', $preview));
 		// Decide how large the preview text should be
 		$preview_max_len = $limit;
 		$preview_len = strlen($preview);
@@ -54,7 +58,7 @@ class phpbbutils {
 				$preview = substr($preview, 0, $clean_break_pos);
 			}
 			$preview .= ' ...';
-		}
+		}*/
 		return $preview;
 	}
 
@@ -68,7 +72,36 @@ class phpbbutils {
 	 */
 	private static function bbcodeStripping($text, $uid)
 	{
-
 		return strip_bbcode($text, $uid);
+	}
+	
+	private static function cut_html ($html, $limit) {
+	    $dom = new DOMDocument();
+	    $dom->loadHTML(mb_convert_encoding("<div>{$html}</div>", "HTML-ENTITIES", "UTF-8"), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+	    self::cut_html_recursive($dom->documentElement, $limit);
+	    return substr($dom->saveHTML($dom->documentElement), 5, -6);
+	}
+
+	private static function cut_html_recursive ($element, $limit) {
+	    if($limit > 0) {
+		if($element->nodeType == 3) {
+		    $limit -= strlen($element->nodeValue);
+		    if($limit < 0) {
+			$element->nodeValue = substr($element->nodeValue, 0, strlen($element->nodeValue) + $limit);
+		    }
+		}
+		else {
+		    for($i = 0; $i < $element->childNodes->length; $i++) {
+			if($limit > 0) {
+			    $limit = self::cut_html_recursive($element->childNodes->item($i), $limit);
+			}
+			else {
+			    $element->removeChild($element->childNodes->item($i));
+			    $i--;
+			}
+		    }
+		}
+	    }
+	    return $limit;
 	}
 }
