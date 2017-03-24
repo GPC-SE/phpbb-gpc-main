a9a95ad2e18bb484ea4fe4d596871efe4a6820d4
Robert Heim <Robert.Heim@rwth-aachen.de>
2017-03-24 21:45:13 +0100

    Update phpbbutils.php

--- util/phpbbutils.php
+++ util/phpbbutils.php
@@ -12,7 +12,7 @@ namespace gpc\main\util;
 class phpbbutils {
 
 	/**
-	 * Trims bbcode containing text to $limit characters by removing all bbcodes.
+	 * Trims bbcode containing text to $limit characters by translating/removing bbcodes.
 	 *
 	 * @param String $post_text the text of the post to trim
 	 * @param String $bbcode_uid the bbcode_uid of the post row
@@ -21,44 +21,15 @@ class phpbbutils {
 	 * @return the trimmed preview text.
 	 *
 	 * @static
-	 * @author MichaelC, Robert Heim
 	 */
 	public static function make_preview_text_from_post($post_text, $bbcode_uid, $bbcode_bitfield, $limit)
 	{
 		$preview = $post_text;
-		// remove youtube completely
-		//$preview = trim(preg_replace('/(\[youtube\])(.*)(\[\/youtube\])/i', '', $preview));
-		//$preview = self::bbcodeStripping($preview, $bbcode_uid);
 		$bbcode_options = OPTION_FLAG_BBCODE + OPTION_FLAG_SMILIES + OPTION_FLAG_LINKS;
 		$preview = generate_text_for_display($preview, $bbcode_uid, $bbcode_bitfield, $bbcode_options);
 		$preview = self::cut_html($preview, $limit);
-		/*$preview = trim(preg_replace('#http(?:\:|&\#58;)//\S+#', '', $preview));
-		// Decide how large the preview text should be
-		$preview_max_len = $limit;
-		$preview_len = strlen($preview);
-		if ($preview_len > $preview_max_len) {
-			// we will add " ..." in the end -> 4 characters
-			$preview_max_len -= 4;
-			// If the first thing is a link, nuke it
-			$preview_clean = str_replace('&#58;', ':', $preview);
-			if (substr($preview_clean, 0, 8) == 'https://' || substr($preview_clean, 0, 7) == 'http://') {
-				$preview = preg_replace('/^(\S*)\s/', '', $preview);
-			}
-			// Truncate to the maximum length
-			$preview = substr($preview, 0, $preview_max_len);
-
-			// See if there is a nice place to break close to the max length
-			$breakchars = array(' ', ',', '.');
-			$clean_break_pos = 0;
-			foreach ($breakchars as $char) {
-				$clean_break_pos_new = strrpos($preview, $char);
-				$clean_break_pos = $clean_break_pos_new > $clean_break_pos ? $clean_break_pos_new : $clean_break_pos;
-			}
-			if ($clean_break_pos) {
-				$preview = substr($preview, 0, $clean_break_pos);
-			}
-			$preview .= ' ...';
-		}*/
+		// remove youtube objects
+		$preview = preg_replace('/<object(.*)<\/object>/is', '', $preview);
 		return $preview;
 	}
 
